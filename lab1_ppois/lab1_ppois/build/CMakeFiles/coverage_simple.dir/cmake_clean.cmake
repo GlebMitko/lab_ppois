afc0f1a8bdda0cb435712f689d77c11dd4007223
Глеб Митько <glebmitko@Noutbuk-Gleb.lan>
2025-12-20 00:02:03 +0300

    chore: ignore IDE, build, and macOS files

--- lab1_ppois/lab1_ppois/build/CMakeFiles/coverage_simple.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/coverage_simple"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/coverage_simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lab2_ppois/lab1_minimarket/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/glebmitko/Desktop/lab_ppois/lab2_ppois/lab1_minimarket
-# Build directory: /Users/glebmitko/Desktop/lab_ppois/lab2_ppois/lab1_minimarket/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/Users/glebmitko/Desktop/lab_ppois/lab2_ppois/lab1_minimarket/build/run_tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/glebmitko/Desktop/lab_ppois/lab2_ppois/lab1_minimarket/CMakeLists.txt;102;add_test;/Users/glebmitko/Desktop/lab_ppois/lab2_ppois/lab1_minimarket/CMakeLists.txt;0;")
